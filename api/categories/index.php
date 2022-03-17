bd3a8fd83bf038a80436e4394666810f4303f54f
Evan Walter <evanwalter@yahoo.com>
2022-03-16 20:41:01 -0500

    troubleshooting POSTs

--- api/categories/index.php
+++ api/categories/index.php
@@ -3,6 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
 
@@ -27,7 +28,6 @@ $validator = new Validator();
 if ($request_method=="GET"){
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-
 				
     // Get query result & row count
     if ($id != NULL){
@@ -95,6 +95,8 @@ if($request_method=="POST"){
     }
 
 if($request_method=="PUT"){
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: PUT');
 
     $data = json_decode(file_get_contents("php://input"));
